104e07177f8b42c710cec55ae55d6fa3ea10659e
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-06-30 23:26:37 +0530

    creating logo and avatar

--- src/Nav.js
+++ src/Nav.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./Nav.css"
 
 function Nav() {
+    const [show, handleShow] = useState(false)
+
+    const transitionNavBar = () =>{
+        if(window.scrollY > 100)
+        handleShow(true);
+        else{
+            handleShow(false)
+        }
+    };
+
+    useEffect(() =>{
+        window.addEventListener("scroll", transitionNavBar);
+        return () => window.removeEventListener("scroll", transitionNavBar)
+
+
+    }, [])
+
     return (
-        <div className="nav">
-            <h1>This is nav</h1>
+        <div className="nav nav__black">
+            <div className="nav__contents">
+            <img  className="nav__logo"
+            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+            alt="netflix logo" />
+
+            <img className="nav__avatar" 
+            src="https://www.pinclipart.com/picdir/middle/547-5474602_character-avatar-clipart.png" alt="" />
             
+            </div>   
         </div>
     )
 }
