b5f3239f2d2b345b4bc02842e52b8b6f21b7e1ad
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-01 00:39:28 +0530

    Creating and adding banner component

--- src/Banner.js
+++ src/Banner.js
@@ -1,15 +1,30 @@
 import React from 'react'
 import "./Banner.css"
 function Banner() {
+
+    function truncate(string, n){
+        return string?.length > n ? string.substr(0, n-1) + '...' : string;
+    }
+
     return (
         <header
          className="banner"
          style={{
              backgroundSize: "cover",
-             backgroundImage: `url("https://torranceca.files.wordpress.com/2019/10/netflix.jpg")`,
+             backgroundImage: `url("https://i.pinimg.com/736x/62/cb/b8/62cbb8138fd6c38a72198e09859a5426.jpg")`,
              backgroundPosition: "center center"
          }}
-         ></header>
+         >
+<div className="banner__contents">
+<h1 className="banner__title">Movie Name</h1>
+<div className="banner__buttons">
+    <button className="banner__button">Play</button>
+    <button className="banner__button">My List</button>
+</div>
+<h1 className="banner__description">{truncate(`This is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test DescriptionThis is Test Description`,150)}</h1>
+</div>
+<div className="banner--fadeBottom"/>
+         </header>
     )
 }
 
