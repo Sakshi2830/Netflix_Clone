df1dfe27be1035c1c4806ec3d1b03e53ec4ebde6
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-02 19:50:50 +0530

    adding firebase and react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import HomeScreen from './HomeScreen';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
 
 
 import './App.css';
 
 function App() {
   return (
-    <div className="app">
-      
-      <HomeScreen/>
+    <div className="app">  
+    <Router>
+      <Switch>
+          <Route exact path="/">
+          <HomeScreen/>
+          </Route>
+        </Switch>
+    </Router>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXfGzoTm1xCDPRruOKbD8ZkkeqAlxAbi8",
+    authDomain: "netflix-clone-7a4f1.firebaseapp.com",
+    projectId: "netflix-clone-7a4f1",
+    storageBucket: "netflix-clone-7a4f1.appspot.com",
+    messagingSenderId: "1059788927080",
+    appId: "1:1059788927080:web:b67945030b39347056cbdd"
+  };
