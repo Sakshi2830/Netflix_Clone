7bcede7e8d09a945802c2bcc0bf85b5a5a1c93ac
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-03 15:11:30 +0530

    Adding firebase  auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCXfGzoTm1xCDPRruOKbD8ZkkeqAlxAbi8",
     authDomain: "netflix-clone-7a4f1.firebaseapp.com",
@@ -6,3 +8,13 @@ const firebaseConfig = {
     messagingSenderId: "1059788927080",
     appId: "1:1059788927080:web:b67945030b39347056cbdd"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+  const auth = firebase.auth();
+
+  export {auth};
+
+  export default db;
