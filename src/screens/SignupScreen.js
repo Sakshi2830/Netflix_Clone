e141d088c7cc2c77b216bd4978077b7acd991a81
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-03 15:29:47 +0530

    set signup and signin credentials

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { auth } from '../firebase';
 import "./SignupScreen.css"
 
 function SignInScreen() {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+
     const register = (e) => {
         e.preventDefault();
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then((authUser) =>{
+            console.log(authUser);
+        })
+            .catch((error) =>{
+                alert(error.message);
+            })
     }
 
     const signIn = (e) =>{
         e.preventDefault();
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) =>{
+            console.log(authUser)
+        }).catch((error) =>{
+            alert(error.message)
+        })
     }
 
 
@@ -15,8 +38,8 @@ function SignInScreen() {
         <div className="signupScreen">
            <form>
            <h1>Sign In</h1>
-           <input type="email" placeholder="Email" />
-           <input type="password" placeholder="Password" />
+           <input ref={emailRef}  type="email" placeholder="Email" />
+           <input ref={passwordRef}  type="password" placeholder="Password" />
            <button type="submit" onClick={signIn}>Sign In</button>
            <h5>Login with Facebook</h5>
            <h4>
