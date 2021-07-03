49314dffd4afc66cb0f717fff01a1ea78418517d
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-03 23:30:45 +0530

    adding user navigation through redux

--- src/App.js
+++ src/App.js
@@ -8,21 +8,29 @@ import {
 import './App.css';
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 function App() {
-const user = null;
+const user = useSelector(selectUser);
+const dispatch = useDispatch();
 
 useEffect(() =>{
  const unsubscribe= auth.onAuthStateChanged((userAuth) => {
     if(userAuth) {
       //logged In
       console.log(userAuth);
+      dispatch(login({
+        uid: userAuth.uid,
+        email: userAuth.email,
+      }))
     }else{
       //logged Out
+      dispatch(logout); 
     }
   })
   return unsubscribe
-},[])
+},[dispatch])
 
   return (
     <div className="app">  
--- src/Nav.js
+++ src/Nav.js
@@ -23,7 +23,7 @@ function Nav() {
         <div className={`nav ${show && "nav__black"}`}>
             <div className="nav__contents">
             <img  className="nav__logo"
-            src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1200px-Netflix_2015_logo.svg.png" 
             alt="netflix logo" />
 
             <img className="nav__avatar" 
