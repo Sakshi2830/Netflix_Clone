c8776e0ed2f21c6ea59b72303d55964536181c84
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-03 15:39:07 +0530

    holding up the user information

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import HomeScreen from './screens/HomeScreen';
 import {
   BrowserRouter as Router,
@@ -7,9 +7,22 @@ import {
 } from "react-router-dom";
 import './App.css';
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
 
 function App() {
 const user = null;
+
+useEffect(() =>{
+ const unsubscribe= auth.onAuthStateChanged((userAuth) => {
+    if(userAuth) {
+      //logged In
+      console.log(userAuth);
+    }else{
+      //logged Out
+    }
+  })
+  return unsubscribe
+},[])
 
   return (
     <div className="app">  
