6ab2a14ab50d60d07f40a3afe7226c59ae5ab213
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-03 13:21:52 +0530

    adding onClick event

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -2,17 +2,26 @@ import React from 'react'
 import "./SignupScreen.css"
 
 function SignInScreen() {
+    const register = (e) => {
+        e.preventDefault();
+    }
+
+    const signIn = (e) =>{
+        e.preventDefault();
+    }
+
+
     return (
         <div className="signupScreen">
            <form>
            <h1>Sign In</h1>
            <input type="email" placeholder="Email" />
            <input type="password" placeholder="Password" />
-           <button type="submit">Sign In</button>
+           <button type="submit" onClick={signIn}>Sign In</button>
            <h5>Login with Facebook</h5>
            <h4>
                <span className="signupScreen__gray">New to Netflix? </span> 
-               <span className="signupScreen__link">Sign up now.</span>
+               <span className="signupScreen__link" onClick={register}>Sign up now.</span>
             </h4>
             
             <h6 className="signupScreen__info">This page is protected by Google reCAPTCHA to ensure you're not a bot.
