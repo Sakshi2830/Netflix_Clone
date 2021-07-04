93349da9f5c5c2bc9e6f0e4e986408b6b85c1dd8
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-04 12:06:05 +0530

    Adding style to profile screen

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux'
 import Nav from '../Nav'
 import {selectUser} from "../features/userSlice"
 import {auth} from "../firebase"
+import "./ProfileScreen.css"
 
 function ProfileScreen() {
     const user = useSelector(selectUser)
@@ -12,17 +13,19 @@ function ProfileScreen() {
             <Nav />
             <div className="profileScreen__body">
                 <h1>Edit Profile</h1>
-            </div>
+            
             <div className="profileScreen__info">
                 <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="avatar" />
                 <div className="profileScreen__details">
                     <h2>{user.email}</h2>
                     <div className="profileScreen__plans">
+                        <h3>Plans</h3>
                         <button 
                         onClick={() => auth.signOut()}
                         className="profileScreen__signout">Sign Out</button>
                     </div>
                 </div>
+            </div>
             </div>
         </div>
     )
