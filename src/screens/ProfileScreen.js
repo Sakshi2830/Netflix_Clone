77a65fcc242fe699d01c2469fa455abaf4d149f7
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-07-11 23:48:17 +0530

    a bug fix

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -14,7 +14,7 @@ function ProfileScreen() {
         <div className="profileScreen">
             <Nav />
             <div className="profileScreen__body">
-                <h1>Edit Profile</h1>
+                <h1>Who is Watching?</h1>
             
             <div className="profileScreen__info">
                 <img src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png" alt="avatar" />
